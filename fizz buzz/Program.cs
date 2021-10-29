7b99a717ea42599fd029631240ce9a62ec6a92d2
Misha <19fishka96@mail.ru>
2021-10-29 08:17:44 +0300

    проба

---
+++ fizz buzz/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace fizz_buzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fizz = "fizz";
+            string buzz = "buzz";
+            string fizz_buzz = "fizz buzz";
+            Console.WriteLine("Введите число итераций < 100");
+            int N = Convert.ToInt32(Console.ReadLine());
+            if (N < 100)
+            {
+                for (int i = 1; i < N; i++)
+                {
+                    if (i % 3 == 0 && i % 5 == 0)
+                    {
+                        Console.WriteLine(fizz_buzz);
+                    }
+
+                    else if (i % 5 == 0)
+                    {
+                        Console.WriteLine(buzz);
+                    }
+                    else if (i % 3 == 0)
+                    {
+
+                        Console.WriteLine(fizz); ;
+
+                    }
+                    else
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Ваше число => 100");
+            }
+        }
+    }
+}
